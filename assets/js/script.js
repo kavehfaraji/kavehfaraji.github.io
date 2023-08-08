07531fd999661fff05533fef209d9241a03af210
kavehfaraji <56932476+kavehfaraji@users.noreply.github.com>
2023-08-08 12:29:40 +0330

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,16 +206,4 @@ $(function () {
         shuffleme.init(); //filter portfolio
     };
 
-    function SendMail() {
-        var params = {
-            from_name: document.getElementById("fullname").value,
-            email_id: document.getElementById("email_id").value,
-            message: document.getElementById("message").value,
-        }
-        emailjs.send("service_upt0kez", "template_i0jeedv", params).then(function (res) {
-            alert("Success! " + res.status);
-        })
-    }
 }());
-
-
